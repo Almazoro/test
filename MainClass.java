053070918d77b5aa621dfe046878699977837095
0hgt0 <max_stalk@rambler.ru>
2016-05-11 14:17:34 +0500

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,11 @@
+public class MainClass {
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        HelloWorld hello = new HelloWorld();
+        hello.say("How do you do?");
+        hello.setName("John");
+        hello.setFamilyName("Jackson");
+        hello.sayToPerson("how do you do?", hello.getName());
+        hello.getFamilyName();
+    }
+}
