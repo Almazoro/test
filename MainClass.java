fe09f1172bf3254888d925a8117cce8d1f22125f
Almazoro <almaz_oro2@mail.ru>
2016-05-11 14:19:33 +0500

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,6 +1,5 @@
 public class MainClass {
     public static void main(String[] args) {
-// TODO Auto-generated method stub
         HelloWorld hello = new HelloWorld();
         hello.say("How do you do?");
         hello.setName("John");
